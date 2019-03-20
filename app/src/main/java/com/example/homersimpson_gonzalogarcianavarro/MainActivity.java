35606c91bd0377a4ba37a03e3d84db1e0c113cc5
Gonzalo <gonzalo.garcia@copernic.cat>
2019-03-20 11:20:01 +0100

    Animación TheSimpson(AnimationList) Funcional , Agregadas imágenes.

--- app/src/main/java/com/example/homersimpson_gonzalogarcianavarro/MainActivity.java
+++ app/src/main/java/com/example/homersimpson_gonzalogarcianavarro/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.homersimpson_gonzalogarcianavarro;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Animación Título TheSimpson-----------------------------------------
+        ImageView imageView = findViewById(R.id.imageTheSimpsonTitle);
+        imageView.setBackgroundResource(R.drawable.animation_list_thesimpson);
+        animationDrawable = (AnimationDrawable) imageView.getBackground();
+        animationDrawable.start();
+        //--------------------------------------------------------------------
     }
 }
