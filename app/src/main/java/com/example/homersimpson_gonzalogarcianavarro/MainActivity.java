1e10367c629b7bcde9089543e1d0056b38d5f130
Gonzalo <gonzalo.garcia@copernic.cat>
2019-03-20 13:13:35 +0100

    Animaciones de las imágenes

--- app/src/main/java/com/example/homersimpson_gonzalogarcianavarro/MainActivity.java
+++ app/src/main/java/com/example/homersimpson_gonzalogarcianavarro/MainActivity.java
@@ -6,6 +6,8 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 
@@ -45,21 +47,72 @@ public class MainActivity extends AppCompatActivity {
         imageViewOjo.setVisibility(View.INVISIBLE);
         //---------------------------------------------------------------------
         //On Click Listener de título TheSimpson
-        imageViewTheSimpson.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    imageViewDonut.setVisibility(View.VISIBLE);
-                    imageViewERojo.setVisibility(View.VISIBLE);
-                    imageViewEAzul.setVisibility(View.VISIBLE);
-                    imageViewEVerde.setVisibility(View.VISIBLE);
-            }
-        });
+        player = MediaPlayer.create(this, R.raw.the_simpsons);
+    }
+
+    public void clockwise(View view){
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.clockwise);
+        view.startAnimation(animation);
+    }
+
+    public void clockwise2(View view){
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.clockwise2);
+        view.startAnimation(animation);
+    }
+
+    public void clockwise3(View view){
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.clockwise3);
+        view.startAnimation(animation);
+    }
+
+    public void slide(View view){
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.slide);
+        view.startAnimation(animation);
     }
 
     public void playSimpsonSong(View v) {
-        player = MediaPlayer.create(this, R.raw.the_simpsons);
         if (player.isPlaying()) {
             player.pause();
-        }else {player = MediaPlayer.create(this, R.raw.the_simpsons);player.start();}
+        }else {player.start();}
+    }
+
+
+    public void aparecer(View view) {
+        if(imageViewEVerde.getVisibility() == View.VISIBLE) {
+            imageViewDonut.setVisibility(View.INVISIBLE);
+            imageViewDonut.clearAnimation();
+
+            imageViewERojo.setVisibility(View.INVISIBLE);
+            imageViewERojo.clearAnimation();
+
+            imageViewEAzul.setVisibility(View.INVISIBLE);
+            imageViewEAzul.clearAnimation();
+
+            imageViewEVerde.setVisibility(View.INVISIBLE);
+            imageViewEVerde.clearAnimation();
+
+            imageViewOjo.setVisibility(View.INVISIBLE);
+            imageViewOjo.clearAnimation();
+
+        }else{
+            imageViewDonut.setVisibility(View.VISIBLE);
+            slide(imageViewDonut);
+
+            imageViewERojo.setVisibility(View.VISIBLE);
+            clockwise(imageViewERojo);
+
+            imageViewEAzul.setVisibility(View.VISIBLE);
+            clockwise(imageViewEAzul);
+
+            imageViewEVerde.setVisibility(View.VISIBLE);
+            clockwise2(imageViewEVerde);
+
+            imageViewOjo.setVisibility(View.VISIBLE);
+            clockwise3(imageViewOjo);
+        }
     }
 }
