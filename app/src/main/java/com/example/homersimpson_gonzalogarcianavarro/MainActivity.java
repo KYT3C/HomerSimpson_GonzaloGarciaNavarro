e251eabe70e5a874fde9fffec7c8c47bc48b77a5
Gonzalo <gonzalo.garcia@copernic.cat>
2019-03-20 11:49:19 +0100

    El título de TheSimpson hace aparecer las imágenes.

--- app/src/main/java/com/example/homersimpson_gonzalogarcianavarro/MainActivity.java
+++ app/src/main/java/com/example/homersimpson_gonzalogarcianavarro/MainActivity.java
@@ -3,22 +3,53 @@ package com.example.homersimpson_gonzalogarcianavarro;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //AnimationList
     AnimationDrawable animationDrawable;
+    //ImageViewImagenesAparecen
+    ImageView imageViewTheSimpson,imageViewDonut,imageViewERojo,imageViewEAzul,imageViewEVerde,imageViewOjo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //Animación Título TheSimpson-----------------------------------------
-        ImageView imageView = findViewById(R.id.imageTheSimpsonTitle);
-        imageView.setBackgroundResource(R.drawable.animation_list_thesimpson);
-        animationDrawable = (AnimationDrawable) imageView.getBackground();
+        imageViewTheSimpson = findViewById(R.id.imageTheSimpsonTitle);
+        imageViewTheSimpson.setBackgroundResource(R.drawable.animation_list_thesimpson);
+        animationDrawable = (AnimationDrawable) imageViewTheSimpson.getBackground();
         animationDrawable.start();
         //--------------------------------------------------------------------
+        //ImageViewImagenesAparecen
+        imageViewDonut = findViewById(R.id.imageViewDonut);
+        imageViewDonut.setVisibility(View.INVISIBLE);
+
+        imageViewERojo = findViewById(R.id.imageViewRojo);
+        imageViewERojo.setVisibility(View.INVISIBLE);
+
+        imageViewEAzul = findViewById(R.id.imageViewAzul);
+        imageViewEAzul.setVisibility(View.INVISIBLE);
+
+        imageViewEVerde = findViewById(R.id.imageViewVerde);
+        imageViewEVerde.setVisibility(View.INVISIBLE);
+
+        imageViewOjo = findViewById(R.id.imageViewOjo);
+        imageViewOjo.setVisibility(View.INVISIBLE);
+        //---------------------------------------------------------------------
+        //On Click Listener de título TheSimpson
+        imageViewTheSimpson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageViewDonut.setVisibility(View.VISIBLE);
+                imageViewERojo.setVisibility(View.VISIBLE);
+                imageViewEAzul.setVisibility(View.VISIBLE);
+                imageViewEVerde.setVisibility(View.VISIBLE);
+            }
+        });
+
     }
+
 }
